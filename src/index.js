f409b56591b7187e1af72d3cc801aa7a74a90390
elveret <lena.veret@gmail.com>
2021-03-09 19:39:40 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+  let close = [];
+  let counter = '';
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+  }
+
+  for (let char of str) {
+      if (open.includes(char) && (open.indexOf(char) !== close.indexOf(char))) {
+          counter += char;
+      } else if (open.includes(char) && (open.indexOf(char) === close.indexOf(char))) {
+          if (counter.length !== 0 && (counter[counter.length - 1] === char)) {
+              counter = counter.slice(0, (counter.length - 1));
+          } else {
+              counter += char;
+          }
+      } else {
+          if (close.includes(char) && (close.indexOf(char) == open.indexOf(counter[counter.length - 1]))) {
+              counter = counter.slice(0, (counter.length - 1));
+          } else {
+              return false;
+          }
+      }
+  }
+  return (counter.length === 0) ? true : false;
 }
